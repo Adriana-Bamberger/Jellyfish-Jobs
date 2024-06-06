fcf872cc4b58b6576f26cd663afbeca1e1db5d06
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-07 00:44:12 +1200

    Add new functions: addNewJob, getJobById

--- server/db/routes/jobs.ts
+++ server/db/routes/jobs.ts
@@ -1,5 +1,5 @@
 import db from '../connection.ts'
-import { dbJob, JobData } from '../../../models/job.ts'
+import { dbJob, dbJobData, Job, JobData } from '../../../models/job.ts'
 
 const columns = [
   'id',
@@ -17,6 +17,28 @@ const columns = [
   'role',
 ]
 
+const mainJobsColumns = [
+  'id',
+  'company',
+  'logo',
+  'logo_background as logoBackground',
+  'position',
+  'posted_at as postedAt',
+  'contract',
+  'location',
+]
+
 export async function getAllJobs(): Promise<dbJob[]> {
-  return db('jobs').select(...columns)
+  return db('jobs').select(...mainJobsColumns)
+}
+
+export async function getJobById(id: number): Promise<dbJob> {
+  return db('jobs')
+    .select(...columns)
+    .where({ id })
+    .first()
+}
+
+export async function addNewJob(newJobData: dbJobData): Promise<number> {
+  return db('jobs').insert({ ...newJobData }, ['id'])
 }
--- server/routes/jobs.ts
+++ server/routes/jobs.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 import * as db from '../db/routes/jobs.ts'
 
-import { Job } from '../../models/job.ts'
+import { Job, JobData, dbJobData } from '../../models/job.ts'
 
 const router = Router()
 
@@ -10,16 +10,51 @@ router.get('/', async (req, res) => {
   try {
     const jobs = await db.getAllJobs()
 
-    const parsedJobs = jobs.map((job) => {
-      const { requirements, role, ...rest } = job
-      return {
-        ...rest,
-        requirements: JSON.parse(requirements),
-        role: JSON.parse(role),
-      }
-    }) as Job[]
+    // const parsedJobs = jobs.map((job) => {
+    //   const { requirements, role, ...rest } = job
+    //   return {
+    //     ...rest,
+    //     requirements: JSON.parse(requirements),
+    //     role: JSON.parse(role),
+    //   }
+    // }) as Job[]
 
-    res.json(parsedJobs)
+    res.json(jobs)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.get('/:id', async (req, res) => {
+  try {
+    const id = Number(req.params.id)
+    const job = await db.getJobById(id)
+    const { requirements, role, ...rest } = job
+    const parsedJob = {
+      ...rest,
+      requirements: JSON.parse(requirements),
+      role: JSON.parse(role),
+    }
+    res.json(parsedJob)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Something went wrong' })
+  }
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const { requirements, role, logoBackground, postedAt, ...rest } = req.body
+    const newJobRecord: dbJobData = {
+      ...rest,
+      logo_background: logoBackground,
+      posted_at: postedAt,
+      requirements: JSON.stringify(requirements),
+      role: JSON.stringify(role),
+    }
+    const newJobId = await db.addNewJob(newJobRecord)
+    res.json(newJobId)
   } catch (error) {
     console.log(error)
     res.status(500).json({ message: 'Something went wrong' })
