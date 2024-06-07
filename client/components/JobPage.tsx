194d64596e9544ed198ddfdd996597d8446461b4
Aimee Kilmartin <aimeekilmartin@gmail.com>
2024-06-07 13:22:11 +1200

    working on job page css

--- client/components/JobPage.tsx
+++ client/components/JobPage.tsx
@@ -1,3 +1,4 @@
+import { LINK } from 'superagent'
 import { useJobById } from '../hooks/useJobs'
 import { useParams } from 'react-router-dom'
 
@@ -25,8 +26,8 @@ export default function JobPage() {
               {data.logoBackground}
               <h2>{data.company}</h2>
               <p>{data.website}</p>
-              <button className="inline-flex items-center rounded-md bg-indigo-600 px-1 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-                {data.apply}
+              <button className="inline-flex items-center rounded-md bg-indigo-400 px-1 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <p>Company Site</p>
               </button>
             </div>
             <div className="mt-16 grid grid-cols-1 gap-x-1 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
@@ -38,6 +39,9 @@ export default function JobPage() {
               <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
                 {data.position}
               </h1>
+              <button className="inline-flex items-center rounded-md bg-indigo-600 px-1 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <p>Apply</p>
+              </button>
               <p className="mt-2 text-m text-purple-700 font-semibold">
                 {data.location}
               </p>
@@ -58,7 +62,9 @@ export default function JobPage() {
             <div>
               <p className="mt-2 text-sm text-gray-500">{data.company}</p>
               <h3>{data.position}</h3>
-              <button>{data.apply}</button>
+              <button className="inline-flex items-center rounded-md bg-indigo-600 px-1 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                <p>Apply</p>
+              </button>
             </div>
           </div>
         </main>
