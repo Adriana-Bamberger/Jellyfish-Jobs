d3ecc040619d26679aa318cfbfca09c0a49bc273
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-07 12:00:14 +1200

    Add styling to main app

--- client/components/JobCard.tsx
+++ client/components/JobCard.tsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 import { Job } from '../../models/job'
 
 export default function JobCard(job: Job) {
+  const bgColor = 'bg-[color:' + job.logoBackground + ']'
   return (
     <>
       <div className="bg-white shadow-md rounded-md border border-gray-200 overflow-hidden p-4">
-        <img src={job.logo} alt={job.company} />
+        <div className="bg-red-50 h-16 w-16">
+          <img src={job.logo} alt={job.company} className="h-16" />
+        </div>
         <span className="text-gray-700">
           {job.postedAt}
           {job.contract}
