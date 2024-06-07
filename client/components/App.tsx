de96b8711ebc0a2d2a8de2811eda7f9374c333df
Alex Izmaylov <18300654+DekhanFraser@users.noreply.github.com>
2024-06-07 12:46:45 +1200

    Add searchBar component

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react'
 import { useJobs } from '../hooks/useJobs.ts'
 import JobCard from './JobCard.tsx'
+import { SearchBar } from './SearchBar.tsx'
 
 function App() {
+  const [searchItem, setSearchItem] = useState('')
+  // const [filteredJobs, setFilteredJobs] = useState<Job[]>()
   const { data, isLoading, isError } = useJobs()
 
   if (isLoading) {
@@ -12,17 +16,28 @@ function App() {
     return <p>Error...</p>
   }
 
-  return (
-    <>
-      <div className="app">
-        <div className="header">This is the header</div>
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
-          {data && data.map((job) => <JobCard key={job.id} {...job} />)}
+  if (data) {
+    let filteredData
+    // if (searchItem !== '') {
+
+    // }
+    filteredData = data.filter((job) => {
+      return job.position.toLowerCase().includes(searchItem.toLowerCase())
+    })
+    return (
+      <>
+        <SearchBar />
+        <div className="app">
+          <div className="header">This is the header</div>
+          <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 md:grid-cols-2 lg:grid-cols-3">
+            {filteredData &&
+              filteredData.map((job) => <JobCard key={job.id} {...job} />)}
+          </div>
+          <div className="footer">This is the footer</div>
         </div>
-        <div className="footer">This is the footer</div>
-      </div>
-    </>
-  )
+      </>
+    )
+  }
 }
 
 export default App
